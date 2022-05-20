c2cb53e719d6e428003e9d1e7a1eab0f04c0d3dc
eds9nsilva <antonioedson99@gmail.com>
2022-05-20 00:04:17 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Container,
+  Header,
+  UserAvatar,
+  UserAvatarButton,
+  UserGreeting,
+  UserInfo,
+  UserInfoDetail,
+  UserName,
+  UserWrapper,
+  Icon,
+} from './styles';
+import avatarDefault from '../../assets/avatar02.png';
+
+export const Home: React.FunctionComponent = () => {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <UserAvatarButton onPress={() => {}}>
+              <UserAvatar source={avatarDefault} />
+            </UserAvatarButton>
+            <UserInfoDetail>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Jorge</UserName>
+            </UserInfoDetail>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
+  );
+};
