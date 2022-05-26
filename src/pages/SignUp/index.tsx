3f208adba9320e6d46850fbdc0e603dd6782edab
eds9nsilva <antonioedson99@gmail.com>
2022-05-26 00:11:04 -0300

    style:  implmentacao da logo e botao BackToSgnin

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input/indext';
-import { Container, Content, Title } from './styles';
+import { BackToSgnin, BackToSgninTitle, Container, Content, Icon, Logo, Title } from './styles';
+import logo from '../../assets/logo.png';
 
 export const SignUp: React.FunctionComponent = () => {
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <Container>
-        <Content>
-          <Title>Crie sua conta</Title>
-          <Input placeholder="Nome completo" />
-          <Input placeholder="E-mail" />
-          <Input placeholder="Senha" />
-          <Button title="Criar conta" />
-        </Content>
-      </Container>
-    </ScrollView>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Content>
+            <Logo source={logo} />
+            <Title>Crie sua conta</Title>
+            <Input placeholder="Nome completo" />
+            <Input placeholder="E-mail" />
+            <Input placeholder="Senha" />
+            <Button title="Criar conta" />
+          </Content>
+        </Container>
+      </ScrollView>
+      <BackToSgnin>
+        <Icon name="arrow-left" />
+        <BackToSgninTitle>Voltar para logon</BackToSgninTitle>
+      </BackToSgnin>
+    </KeyboardAvoidingView>
   );
 };
